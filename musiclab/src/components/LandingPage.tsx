96909fe6bf45b4b1654c29616eae9cc0f68a7ef5
Qian He <qhe@moneygram.com>
2021-04-28 11:39:29 -0500

    add typescript

---
+++ musiclab/src/components/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function LandingPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
